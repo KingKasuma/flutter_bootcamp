748fd72b0b87fffae90d2617d41e3b747c5d8b98
Mike <lanselot777hailander@gmail.com>
2020-05-10 22:49:13 -0400

    Passing funciton as parameter of another function

--- bmi-calculator-flutter/lib/input_page.dart
+++ bmi-calculator-flutter/lib/input_page.dart
@@ -33,29 +33,25 @@ class _InputPageState extends State<InputPage> {
           Expanded(child: Row(
             children: <Widget>[
               Expanded(
-                child: GestureDetector(
-                  onTap: (){
+                child: ReusableCard(
+                  onPress: (){
                     setState(() {
                       selectedGender = Gender.male;
                     });
                   },
-                  child: ReusableCard(
-                    colour: selectedGender == Gender.male ? activeCardColour : inactiveCardColour,
-                    cardChild: IconContent(icon: FontAwesomeIcons.mars, label: 'MALE'),
-                  ),
+                  colour: selectedGender == Gender.male ? activeCardColour : inactiveCardColour,
+                  cardChild: IconContent(icon: FontAwesomeIcons.mars, label: 'MALE'),
                 ),
               ),
               Expanded(
-                child: GestureDetector(
-                  onTap: (){
+                child: ReusableCard(
+                  onPress: (){
                     setState(() {
                       selectedGender = Gender.female;
                     });
                   },
-                  child: ReusableCard(
-                    colour: selectedGender == Gender.female ? activeCardColour : inactiveCardColour,
-                    cardChild: IconContent(icon: FontAwesomeIcons.venus, label: 'FEMALE',),
-                  ),
+                  colour: selectedGender == Gender.female ? activeCardColour : inactiveCardColour,
+                  cardChild: IconContent(icon: FontAwesomeIcons.venus, label: 'FEMALE',),
                 ),
               )
             ],
