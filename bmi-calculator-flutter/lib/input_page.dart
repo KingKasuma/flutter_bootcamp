170303f62a4ed8db3b7c24f4be4c351201ae7d95
Mike <lanselot777hailander@gmail.com>
2020-05-10 21:27:26 -0400

    Adding final and const variables

--- bmi-calculator-flutter/lib/input_page.dart
+++ bmi-calculator-flutter/lib/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0;
+const activeCardColour = Color(0XFF1D1E33);
+const bottomContainerColour = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -16,17 +20,23 @@ class _InputPageState extends State<InputPage> {
         children: <Widget>[
           Expanded(child: Row(
             children: <Widget>[
-              Expanded(child: ReusableCard(colour: Color(0XFF1D1E33),),),
-              Expanded(child: ReusableCard(colour: Color(0XFF1D1E33),),)
+              Expanded(child: ReusableCard(colour: activeCardColour,),),
+              Expanded(child: ReusableCard(colour: activeCardColour,),)
             ],
           )),
-          Expanded(child: ReusableCard(colour: Color(0XFF1D1E33)),),
+          Expanded(child: ReusableCard(colour: activeCardColour),),
           Expanded(child: Row(
             children: <Widget>[
-              Expanded(child: ReusableCard(colour: Color(0XFF1D1E33)),),
-              Expanded(child: ReusableCard(colour: Color(0XFF1D1E33)),)
+              Expanded(child: ReusableCard(colour: activeCardColour),),
+              Expanded(child: ReusableCard(colour: activeCardColour),)
             ],
           )),
+          Container(
+            color: bottomContainerColour,
+            margin: EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight ,
+          )
         ],
       )
     );
